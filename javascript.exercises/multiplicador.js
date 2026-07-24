b0ac25f71dd3e48dd61013c6d7022164dedcccab
pacocapython <gabrielgustavosv8@gmail.com>
2026-07-24 10:32:45 -0300

    Delete javascript.exercises directory

--- javascript.exercises/multiplicador.js
+++
@@ -1,5 +0,0 @@
-//9. Multiplicador (Arrow Function Implícita)
-//Crie uma Arrow Function em uma única linha (com retorno implícito, sem chaves e sem return)
-//  chamada dobrar que receba um número e retorne o dobro dele.
-const dobrar = numero => numero *2;
-console.log(dobrar(10))
